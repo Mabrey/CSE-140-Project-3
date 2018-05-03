924eb0581bc53d0e3dc83bf238eba3881b9bba9f
Mabrey <32260556+Mabrey@users.noreply.github.com>
2018-05-03 14:50:28 -0700

    Implemented Loop unrolling

--- proj3/dgemm-optimize.c
+++ proj3/dgemm-optimize.c
@@ -1,17 +1,91 @@
 
 void loopUnroll(int m, int n, float *A, float *C)
 {
+    int multOps = m * n * m;        
+    int addOps = m * n;
     for( int i = 0; i < m; i++ )
         for( int k = 0; k < n; k++ ) 
-            for( int j = 0; j < m; j++ ) 
-	            C[i+j*m] += A[i+k*m] * A[j+k*m];
+            for( int j = 0; j < m; j+= 5 ) {
+                //to start untolling the loop, we need to know how far we can unroll it. 
+                //since we are in the j loop, we should unroll the j loop by 5 or less? 
+                //m is our row/column length so distToColumnEnd will be our boundry for unrolling to prevent reading the next row.
+                int distToColumnEnd = m - j;
+
+                if (distToColumnEnd > 4)    //unroll 5 times
+                {
+                    C[i+j*m] += A[i+k*m] * A[j+k*m];
+                    C[i+(j + 1)*m] += A[i+k*m] * A[(j + 1)+k*m];
+                    C[i+(j + 2)*m] += A[i+k*m] * A[(j + 2)+k*m];
+                    C[i+(j + 3)*m] += A[i+k*m] * A[(j + 3)+k*m];
+                    C[i+(j + 4)*m] += A[i+k*m] * A[(j + 4)+k*m];
+                }
+
+                else if (distToColumnEnd == 4)   //unroll 4 times
+                {
+                    C[i+j*m] += A[i+k*m] * A[j+k*m];
+                    C[i+(j + 1)*m] += A[i+k*m] * A[(j + 1)+k*m];
+                    C[i+(j + 2)*m] += A[i+k*m] * A[(j + 2)+k*m];
+                    C[i+(j + 3)*m] += A[i+k*m] * A[(j + 3)+k*m];
+                }
+                
+                else if (distToColumnEnd == 3) //unroll 3 times
+                {
+                    C[i+j*m] += A[i+k*m] * A[j+k*m];
+                    C[i+(j + 1)*m] += A[i+k*m] * A[(j + 1)+k*m];
+                    C[i+(j + 2)*m] += A[i+k*m] * A[(j + 2)+k*m];
+                } 
+
+                else if (distToColumnEnd == 2)   //unroll 2 times
+                {
+                    C[i+j*m] += A[i+k*m] * A[j+k*m];
+                    C[i+(j + 1)*m] += A[i+k*m] * A[(j + 1)+k*m];
+                }  
+
+                else C[i+j*m] += A[i+k*m] * A[j+k*m];   //normal operation
+   
+            }
+           
 }
+
+void loopUnrollWithSwitch(int m, int n, float *A, float *C)
+{
+    int multOps = m * n * m;        
+    int addOps = m * n;
+    for( int i = 0; i < m; i++ )
+        for( int k = 0; k < n; k++ ) 
+            for( int j = 0; j < m; j+= 5) {
+                //to start untolling the loop, we need to know how far we can unroll it. 
+                //since we are in the j loop, we should unroll the j loop by 5 or less? 
+                //m is our row/column length so distToColumnEnd will be our boundry for unrolling to prevent reading the next row.
+                int distToColumnEnd = m - j;
+                if (distToColumnEnd > 4) distToColumnEnd = 5;
+                    
+                switch (distToColumnEnd){
+                    case 5:
+                        C[i+(j + 4)*m] += A[i+k*m] * A[(j + 4)+k*m];
+
+                    case 4:
+                        C[i+(j + 3)*m] += A[i+k*m] * A[(j + 3)+k*m];
+                    
+                    case 3:
+                        C[i+(j + 2)*m] += A[i+k*m] * A[(j + 2)+k*m];
+
+                    case 2:
+                        C[i+(j + 1)*m] += A[i+k*m] * A[(j + 1)+k*m];
+
+                    default:
+                        C[i+j*m] += A[i+k*m] * A[j+k*m];
+
+                }
+            }         
+}
+
 
 
 void dgemm( int m, int n, float *A, float *C )
 {
-// FILL-IN 
     //loop unrolling 
     loopUnroll(m, n, A, C);
+    //loopUnrollWithSwitch(m, n, A, C);
 
 }
