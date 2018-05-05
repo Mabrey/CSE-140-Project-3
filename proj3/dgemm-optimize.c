949a94b6f529c2a3024eddb1fe7b10d1e1dff655
Mabrey <32260556+Mabrey@users.noreply.github.com>
2018-05-05 01:14:33 -0700

    Finished matrix padding

--- proj3/dgemm-optimize.c
+++ proj3/dgemm-optimize.c
@@ -199,15 +199,15 @@ void reorderingWithUnroll(int m, int n, float *A, float *C){
 
 void matrixPad(int m, int n, float *A, float *C) {
 
-	if (m > n) {
+	if (n % 2 == 1) {
 
 		//make rows = columns so we operate on square matrix
-		int newN = m;
+		int newN = n + 1;
 		//allocate new memory for padded matrix
-		float *paddedA = (float*)malloc(m * m * sizeof(float));
+		float *paddedA = (float*)malloc(m * (newN) * sizeof(float));
 
 		//initialize matrix to 0
-		for (int i = 0; i < (m * m); i++) {
+		for (int i = 0; i < (m * newN); i++) {
 				paddedA[i] = 0;		
 		}
 
@@ -219,30 +219,29 @@ void matrixPad(int m, int n, float *A, float *C) {
 
 		//multiply
 		for (int i = 0; i < m; i++)
-			for (int k = 0; k < m; k++)
+			for (int k = 0; k < newN; k++)
 				for (int j = 0; j < m; j++)
 					C[i + j * m] += paddedA[i + k * m] * paddedA[j + k * m];
 	}
 
-	else if (n > m) {
+	else if (m % 2 == 1) {
 
 		//make colomns = rows so we operate on square matrix
-		int newM = n;
-		int dif = n - m;
-		int offset = 0;
-
+		int newM = m + 1;
+		int dif = 1;
+		
 		//allocate new memory for padded matrix
-		float *paddedA = (float*)malloc(n * n * sizeof(float));
+		float *paddedA = (float*)malloc(newM * n * sizeof(float));
 
-		for (int i = 0; i < (n * n); i++) {
+		for (int i = 0; i < (newM * n); i++) {
 				paddedA[i] = 0;
 		}
 
         int j = 0;
 		for (int i = 0; i < m * n; i++) {
-			if (i % m == 0)
-                j += dif;
-            else 
+			if (i % m == 0 && i !=0)
+                j++;
+            
             paddedA[j] = A[i];
             j++;
 		}
